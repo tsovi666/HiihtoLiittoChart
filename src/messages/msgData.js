34ffb19d223bfbc17f803533d1bb8f87a47ed766
Otto Oikari <otto.oikari@op.fi>
2024-06-17 19:28:59 +0300

    shit from ass

---
+++ src/messages/msgData.js
@@ -0,0 +1,47 @@
+
+export default {
+  de: {
+    title: function(d) { return "Hiihtoliiton Scheisse Index 1941"; },
+    performance: function(d) { return "Hiihtoliiton ist ein Market Reich"; },
+    valuation: function(d) { return "Blitzkrieg"; },
+    shareholders: function(d) { return "Übermensch"; },
+    equity: function(d) { return "Endsieg"; },
+    name: function(d) { return "Führer"; },
+    invested: function(d) { return "Lebensraum"; },
+    months: function(d) { return "Saurekraut"; },
+    month: function(d) { return "Lederhosen"; }
+  },
+  en: {
+    title: function(d) { return "Hiihtoliitto Market Index for the year 2024"; },
+    performance: function(d) { return "Hiihtoliitto Market Performance"; },
+    valuation: function(d) { return "Valuation"; },
+    shareholders: function(d) { return "Shareholders"; },
+    equity: function(d) { return "Equity"; },
+    name: function(d) { return "Name"; },
+    invested: function(d) { return "Invested"; },
+    months: function(d) { return "Months"; },
+    month: function(d) { return "Month"; }
+  },
+  fi: {
+    title: function(d) { return "Hiihtoliiton markkinaindeksi vuodelle 2024"; },
+    performance: function(d) { return "Hiihtoliiton markkinasuoritus"; },
+    valuation: function(d) { return "Valuaatio"; },
+    shareholders: function(d) { return "Osakkeenomistajat"; },
+    equity: function(d) { return "Pääoma"; },
+    name: function(d) { return "Nimi"; },
+    invested: function(d) { return "Sijoitettu"; },
+    months: function(d) { return "Kuukautta"; },
+    month: function(d) { return "Kuukausi"; }
+  },
+  sv: {
+    title: function(d) { return "Hiihtåliitån markkinaindexi vuådelle 2024"; },
+    performance: function(d) { return "Hiihtåliitån markkinasuåritus"; },
+    valuation: function(d) { return "Valuaatiå"; },
+    shareholders: function(d) { return "Åsakkeenåmistajat"; },
+    equity: function(d) { return "Pääåma"; },
+    name: function(d) { return "Nimi"; },
+    invested: function(d) { return "Sijåitettu"; },
+    months: function(d) { return "Kuukautta"; },
+    month: function(d) { return "Kuukausi"; }
+  }
+}
